d903e4a43d71ac8c4e5b834418b4cdf6ffe10959
guimasuko <76081837+guimasuko@users.noreply.github.com>
2023-05-24 18:55:41 -0300

    factors from daily returns

--- code/converting_to_csv_daily.m
+++ code/converting_to_csv_daily.m
@@ -1,3 +1,8 @@
 data = load('C:\Users\Usuario\Dropbox\TAQ_Returns\Daily\A.mat')
 
+days = data.sDailyRet.allDays
+returns = [zeros(4531,1)]
+
+daily_returns = table(daily, returns)
+
 data.sDailyRet.Day_20030102(3)
